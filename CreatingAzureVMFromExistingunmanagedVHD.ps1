f43cde74b312953c2bcdf568e9b4b97b24ceca3b
Chris Frizzell <115434067+SpaceCityCloud@users.noreply.github.com>
2022-10-09 19:08:50 -0500

    Add files via upload

---
+++ CreatingAzureVMFromExistingunmanagedVHD.ps1
@@ -0,0 +1,21 @@
+﻿#Login-AzureRmAccount
+
+#Get-AzureRmSubscription 
+
+#Change the subscription Name! After that you're good to go.
+#Select-AzureRmSubscription -SubscriptionName $subscription
+$nicName ="xyzNIC"
+$vmName = "xyzVM"
+$vmSize = "Standard_DS2_v2"
+$osDiskName = "dskName"
+$location = "East US 2"
+$rgName = "rgName"
+
+$nic = Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName
+
+$osDiskUri = "https://storageAccountName.blob.core.windows.net/vhds/vmVHDtoUse.vhd"
+
+$vmConfig1 = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+$vmConfig2 = Set-AzureRmVMOSDisk -VM $vmConfig1 -Name $vmName -VhdUri $osDiskUri -CreateOption Attach -Windows
+$vmConfig3 = Add-AzureRmVMNetworkInterface -VM $vmConfig2 -Id $nic.Id
+New-AzureRMVM -ResourceGroupName $rgName -Location $location -VM $vmconfig3 
---
+++ Login via ServicePrincipal.ps1
@@ -0,0 +1,4 @@
+﻿$applicationId = "xyz";   #Operator-ID
+$securePassword = "abc" | ConvertTo-SecureString -AsPlainText -Force   #Operator-Key
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
+Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId "a-b-c"     #TenantID
